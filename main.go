038ff23e670ff22f251d7e3d2afeb2194a1f6e4c
jordan <jwald3@brockport.edu>
2022-05-26 20:25:26 -0400

    added documentation

--- main.go
+++ main.go
@@ -8,18 +8,29 @@ import (
 )
 
 func main() {
-	notes := []string{"Notes 1", "Notes 2", "Notes 3", "Notes 4"}
 	var noteLength int
 
-	fmt.Print("Enter the maximum number of notes:")
+	fmt.Print("Enter the maximum number of notes: ")
 	fmt.Scan(&noteLength)
 
+	// init string slice with specified number of elements
+	notes := make([]string, 0, noteLength)
+	fmt.Println()
+
+	// "while(true)" program loop
 	for {
-		fmt.Print("Enter a command and data:")
+		fmt.Print("Enter a command and data: ")
 		scanner := bufio.NewScanner(os.Stdin)
 		scanner.Scan()
 		line := scanner.Text()
+		// separate user input into:
+		// [0] : command
+		// [1] : information (needed for create function)
 		arr := strings.SplitN(line, " ", 2)
+
+		if len(arr) != 2 {
+			arr = append(arr, "")
+		}
 
 		if arr[0] == "exit" {
 			exitProgram()
@@ -41,11 +52,13 @@ func exitProgram() {
 }
 
 func readNoteList(notes []string) {
+	// if there are no notes, terminate function early
 	if len(notes) == 0 {
 		fmt.Println("[Info] Notepad is empty")
 		return
 	}
 
+	// iterate through notes, using index as output label
 	for index, value := range notes {
 		if value == "" {
 			continue
@@ -56,11 +69,14 @@ func readNoteList(notes []string) {
 }
 
 func addNote(note string, notes *[]string, noteLength int) {
+	// remove leading and trailing whitespace from note (ensures cleaned input and no "empty" notes)
 	newNote := strings.TrimSpace(note)
 
+	// Notepad will not accept notes past specified limit
 	if len(*notes) >= noteLength {
 		fmt.Println("[Error] Notepad is full")
 	} else {
+		// prevent empty note input
 		if newNote == "" {
 			fmt.Println("[Error] Missing note argument")
 			return
